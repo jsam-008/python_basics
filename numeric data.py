7445ac81788f125e5e0461c4663e75dbe6ae64c8
sam j <jlatha541@gmail.com>
2025-02-04 21:13:34 +0530

    numeric data which in python

---
+++ numeric data.py
@@ -0,0 +1,32 @@
+""" this is a numeric data type .This data type have three types 
+    1. Integer (int)
+    2. Float (float)
+    3. Complex (complex)
+    """
+  # this to show the data type of the numeric data type
+
+# Integer (int)
+x = 10
+print("Integer:", x, "Type:", type(x))
+
+# Float (float)
+y = 10.5
+print("Float:", y, "Type:", type(y))
+ 
+# Complex (complex)
+z = 3 + 4j
+print("Complex:", z, "Type:", type(z))
+
+#ths is to show the conversion of the numeric data type
+
+# Convert int to float
+a = float(x)
+print("Converted int to float:", a, "Type:", type(a))
+
+# Convert float to int
+b = int(y)
+print("Converted float to int:", b, "Type:", type(b))
+
+# Convert int to complex
+c = complex(x)
+print("Converted int to complex:", c, "Type:", type(c))
